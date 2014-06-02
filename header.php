1e182ee2f5ec5196297e9fbbc1ad0d9457df4260
Bernard Bolter <b@bernardbolter.com>
2014-06-02 00:24:50 -0700

    added favicon link

--- header.php
+++ header.php
@@ -5,6 +5,7 @@
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title><?php wp_title( '|', true, 'right' ); ?></title>
 <link rel="stylesheet" type="text/css" href="<?php bloginfo( 'template_url' ); ?>/style.css" media="screen" />
+<link rel="icon" type="image/ico" href="<?php echo esc_url( home_url( '/' ) ); ?>favicon.ico"/>
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 
